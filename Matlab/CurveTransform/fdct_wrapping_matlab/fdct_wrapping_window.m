199cd879d448e88056981315831f9b76a63ca553
boyLen <1024552631@qq.com>
2021-09-10 10:33:18 +0800

    first commit

--- Matlab/CurveTransform/fdct_wrapping_matlab/fdct_wrapping_window.m
+++ Matlab/CurveTransform/fdct_wrapping_matlab/fdct_wrapping_window.m
@@ -12,24 +12,14 @@ function [wl,wr] = fdct_wrapping_window(x)
 % Used at least in fdct_wrapping.m and ifdct_wrapping.m
 %
 % By Laurent Demanet, 2004
-disp('fdct_wrapping_window');
-disp(size(x));
-disp(x);
 wr = zeros(size(x));
 wl = zeros(size(x));
 x(abs(x) < 2^-52) = 0;
 wr((x > 0) & (x < 1)) = exp(1-1./(1-exp(1-1./x((x > 0) & (x < 1)))));
 wr(x <= 0) = 1;
-
 wl((x > 0) & (x < 1)) = exp(1-1./(1-exp(1-1./(1-x((x > 0) & (x < 1))))));
 wl(x >= 1) = 1;
 normalization = sqrt(wl.^2 + wr.^2);
-disp('boyLen1');
-disp(wl.^2 + wr.^2);
-disp('boyLen1');
-disp(normalization);
-x{909090990};
-
 wr = wr ./ normalization;
 wl = wl ./ normalization;
 
